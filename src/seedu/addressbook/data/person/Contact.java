1951731c072584cbd36b8acaabfbdc7384a82b2e
Brandon <brandonyeoxg@gmail.com>
2016-09-06 18:24:39 +0800

    Add comments and fixed error in ParserTest class

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -2,6 +2,9 @@ package seedu.addressbook.data.person;
 
 import seedu.addressbook.data.exception.IllegalValueException;
 
+/**
+ * Represents a template of a Person's contact in the addressbook
+ */
 public abstract class Contact {
     private final String value;
     private boolean isPrivate;
@@ -14,6 +17,13 @@ public abstract class Contact {
         value = address;
     }
     
+    /**
+     * Checks if the string is valid or not based on the regex.
+     * 
+     * @param test Input string to be tested, should not be null.
+     * @param regex Used to check if the test string is valid.
+     * @return True if it is valid, else false.
+     */
     public static boolean isValid(String test, String regex) {
         return test.matches(regex);
     }
